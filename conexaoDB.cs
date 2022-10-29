6a9ca64ac841bf3a073f31cf2862595408f5e0e9
luidigg <luidigg@gmail.com>
2022-10-29 13:30:08 -0300

    comando de conexão mysql

--- conexaoDB.cs
+++ conexaoDB.cs
@@ -1,7 +1,30 @@
-Console.WriteLine("EAE MANO TUDO JOIA");
+//teste inicial
 
-int a = 2;
-int b = 3;
-int c = 48;
+using MySql.Data.MySqlClient;
 
-Console.WriteLine(a + b + c);
+namespace DAL
+{
+    public class DatabaseService
+    {
+        private MySqlConnection _connection;
+        public DatabaseService()
+        {
+            _connection = new MySqlConnection("server=localhost;port=3306;user=root;password=;database=classicmodels");
+            _connection.Open();
+        }
+
+        public MySqlDataReader Select(string sql)
+        {
+            MySqlCommand command = new MySqlCommand(sql, _connection);
+            return command.ExecuteReader();
+        }
+    }
+
+}
+
+//https://www.devmedia.com.br/como-criar-um-sistema-sistema-de-autenticacao-em-asp-net-e-csharp/31701
+
+// precisamos programar a:
+// > criação de conta por parte do usuário e das oficinas
+// > login de clientes cadastrados
+// > depois partimos pras outras etapas...
