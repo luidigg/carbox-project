ddc1e54b17c668a83f9dd870e94f4b872b7f261e
luidigg <luidigg@gmail.com>
2022-12-02 07:51:24 -0300

    login e listagem de oficinas consertado

--- codfonte.cs
+++ codfonte.cs
@@ -37,26 +37,37 @@ Console.WriteLine();
 
 //tela de login abaixo
 
-Console.ForegroundColor = ConsoleColor.Cyan; //verificar com professor.
+Console.ForegroundColor = ConsoleColor.Cyan;
 
 Console.WriteLine("[a]  -  Criar conta");
 Console.WriteLine("[b]  -  Fazer login");
 Console.WriteLine("");
 Console.ForegroundColor = ConsoleColor.DarkCyan;
 Console.WriteLine("[x]  -  Sair");
+Console.WriteLine("");
 Console.ResetColor();
 
 string opcaoLogin = Console.ReadLine();
 
 
-if(opcaoLogin == "x")
+if (opcaoLogin != "a" && opcaoLogin != "b" && opcaoLogin != "x")
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Nenhuma opção foi selecionada, digite 'a' ou 'b' e tecle Enter");
+    Thread.Sleep(3500);
+    Console.Beep(600, 700);
+    goto RETORNO1;
+}
+
+
+if (opcaoLogin == "x")
 {
     return;
 }
 
 if (opcaoLogin == "")
 {
-    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine("Nenhuma opção foi selecionada, digite 'a' ou 'b' e tecle Enter");
     Thread.Sleep(3500);
     Console.Beep(600, 700);
@@ -65,7 +76,7 @@ if (opcaoLogin == "")
 
 if (opcaoLogin == "a")
 {
-    RETORNO2:
+RETORNO2:
     Console.ResetColor();
     Console.Clear();
     Console.ForegroundColor = ConsoleColor.White;
@@ -81,7 +92,7 @@ if (opcaoLogin == "a")
         goto RETORNO2;
     }
 
-    RETORNO3:
+RETORNO3:
     Console.WriteLine("");
     Console.WriteLine("Insira seu número de telefone");
     string telefoneCliente = Console.ReadLine();
@@ -129,7 +140,7 @@ if (opcaoLogin == "a")
 
     while (true)
     {
-        RETORNO4:
+    RETORNO4:
         Console.ResetColor();
         Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("");
@@ -165,7 +176,7 @@ if (opcaoLogin == "a")
             Console.ResetColor();
         }
 
-        RETORNO5:
+    RETORNO5:
         Console.WriteLine("Insira seu nome de usuário");
         string usernameCliente = Console.ReadLine();
 
@@ -177,7 +188,7 @@ if (opcaoLogin == "a")
             goto RETORNO5;
         }
 
-        RETORNO6:
+    RETORNO6:
         Console.ResetColor();
         Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Insira sua senha");
@@ -199,7 +210,7 @@ if (opcaoLogin == "a")
 
         if (senhaClienteA == senhaClienteB)
         {
-            RETORNOcreate:
+        RETORNOcreate:
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("");
@@ -213,12 +224,12 @@ if (opcaoLogin == "a")
             Console.WriteLine("");
             Console.ResetColor();
             Console.ForegroundColor = ConsoleColor.White;
-            
+
             string confirm = Console.ReadLine();
             int confirmaConta = Convert.ToInt16(confirm);
 
 
-            if(confirmaConta == 0)
+            if (confirmaConta == 0)
             {
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -232,10 +243,10 @@ if (opcaoLogin == "a")
                 goto RETORNO1;
             }
 
-            if(confirmaConta == 1)
+            if (confirmaConta == 1)
             {
                 MySqlCommand objCmd = new MySqlCommand("insert into clientes (CLIENTE_NOME, CLIENTE_NUMERO, DATA_NASCIMENTO, USERNAME_CLIENTE, USERSENHA_CLIENTE, CPF_CLIENTE) values (?, ?, ?, ?, ?, ?)", objcon);
-                
+
                 //parametros
                 objCmd.Parameters.Add("@CLIENTE_NOME", MySqlDbType.VarChar, 100).Value = nomeCliente;
                 objCmd.Parameters.Add("@CLIENTE_NUMERO", MySqlDbType.VarChar, 85).Value = telefoneCliente;
@@ -243,14 +254,15 @@ if (opcaoLogin == "a")
                 objCmd.Parameters.Add("@USERNAME_CLIENTE", MySqlDbType.VarChar, 100).Value = usernameCliente;
                 objCmd.Parameters.Add("@USERSENHA_CLIENTE", MySqlDbType.VarChar, 100).Value = senhaClienteB;
                 objCmd.Parameters.Add("@CPF_CLIENTE", MySqlDbType.VarChar, 45).Value = cpfCliente;
-                
+
                 //executa o insert no banco
                 objCmd.ExecuteNonQuery();
                 Console.Clear();
                 Console.WriteLine("");
                 Console.WriteLine("Conta cadastrada com sucesso!");
+                Console.Beep(1800, 400);
                 break;
-                
+
             }
 
             else
@@ -267,8 +279,9 @@ if (opcaoLogin == "a")
 
 
 
-if (opcaoLogin == "b") //código aqui dando bug, corrigir isso ou partir pra construção dos menus
+if (opcaoLogin == "b") //ATUALIZADO: FUNCIONANDO
 {
+RETORNOlog:
     Console.ResetColor();
     Console.ForegroundColor = ConsoleColor.Yellow;
 
@@ -279,82 +292,186 @@ if (opcaoLogin == "b") //código aqui dando bug, corrigir isso ou partir pra con
     Console.WriteLine("");
     Console.WriteLine("Digite sua senha:");
     string senhaInf = Console.ReadLine();
-    
-    
-    MySqlCommand objCmd = new MySqlCommand("select count (*) from clientes where USERNAME_CLIENTE = ? and USERSENHA_CLIENTE = ?", objcon);
 
-    objCmd.Parameters.Add("@USERNAME_CLIENTE", MySqlDbType.VarChar, 100).Value = userInf;
-    objCmd.Parameters.Add("@USERSENHA_CLIENTE", MySqlDbType.VarChar, 100).Value = userInf;
-    
-    
-    int rows = Convert.ToInt32(objCmd.ExecuteScalar());
 
-    if(rows > 0)
+    MySqlCommand login1 = new MySqlCommand("select USERNAME_CLIENTE from clientes where USERNAME_CLIENTE = ? and USERSENHA_CLIENTE = ?", objcon);
+
+    login1.Parameters.Add("@USERNAME_CLIENTE", MySqlDbType.VarChar, 100).Value = userInf;
+    login1.Parameters.Add("@USERSENHA_CLIENTE", MySqlDbType.VarChar, 100).Value = senhaInf;
+
+    var reader = login1.ExecuteReader();
+
+
+    if (reader.HasRows)
     {
-        Console.WriteLine(rows);
+        //continua para o menu
     }
 
     else
     {
-        Console.WriteLine("ahh nao");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("");
+        Console.WriteLine("Senha ou usuário incorreto");
+        Console.Beep(1300, 400);
+        Console.Beep(1300, 400);
+        Console.ResetColor();
+        goto RETORNOlog;
     }
-    
-
 
 
 
 
 }
 
+objcon.Close();
 
 //tela de login acima
 
-Console.ResetColor();
-Console.ForegroundColor = ConsoleColor.Green;
-
-Console.WriteLine("___________________Menu Pricipal______________________");
-Console.WriteLine();
-Console.WriteLine("__________ Escolha uma das opções listadas: __________");
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine("[1]  -  Oficinas Disponíveis");
-Console.WriteLine("[2]  -  Manutenções Agendadas");
-Console.WriteLine("[3]  -  Mapa de Oficinas | (Google Maps)");
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine("[4]  -  Meu Perfil");
-Console.WriteLine("[5]  -  Meus veículos");
-Console.WriteLine();
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine("[0]  -  Sair do Aplicativo");
-Console.ResetColor();
-
-string opcaoSelecionada = Console.ReadLine();
 
 
-if (opcaoSelecionada == "")
+//MENU ABAIXO
+
+
+
+while (true)
 {
-    Console.BackgroundColor = ConsoleColor.Red;
-    Console.WriteLine("#ERRO 1#  -  Você não selecionou nenhuma opção, selecione uma opção numérica da lista e dê 'enter' -  EX.: '1'");
-}
-Console.ResetColor();
+    RETORNOmenu:
+
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.ResetColor();
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.WriteLine("___________________Menu Pricipal______________________");
+    Console.WriteLine();
+    Console.WriteLine("__________ Escolha uma das opções listadas: __________");
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("[1]  -  Oficinas Disponíveis");
+    Console.WriteLine("[2]  -  Agendar Manutenções");
+    Console.WriteLine("[3]  -  Ajuda com o veículo");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("[4]  -  Meu Perfil");
+    Console.WriteLine("[5]  -  Meus veículos");
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("[0]  -  Sair do Aplicativo");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.ResetColor();
+
+    string opcaoSelecionada = Console.ReadLine();
 
 
-if (opcaoSelecionada == "1") { }
+    if (opcaoSelecionada == "")
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.WriteLine("#ERRO 1#  -  Você não selecionou nenhuma opção, selecione uma opção numérica da lista e dê 'enter' -  EX.: '1'");
+        goto RETORNOmenu;
+    }
+    Console.ResetColor();
 
-if (opcaoSelecionada == "2") { }
 
-if (opcaoSelecionada == "3") { }
+    if (opcaoSelecionada == "1")
+    {
+        MySqlConnection oficinaList = new MySqlConnection("server=localhost;port=3306;user=root; database=mydb; password=;");
+        oficinaList.Open();
 
-if (opcaoSelecionada == "4") { }
+        MySqlCommand option1 = new MySqlCommand("select OFICINA_NOME, OFICINA_NUMERO, OFICINA_LOCALIZACAO from oficinas;", oficinaList);
+        option1.ExecuteNonQuery();
 
-if (opcaoSelecionada == "5") { }
+        MySqlDataReader reader1;
 
-if (opcaoSelecionada == "6") { }
+        reader1 = option1.ExecuteReader();
 
-if (opcaoSelecionada == "0")
-{
-    //break;
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.White;
+
+
+
+        //cabeçalho da tabela        
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        Console.Write("       OFICINA".PadRight(45));
+        Console.Write("TELEFONE".PadRight(45));
+        Console.Write("LINK DE LOCALIZAÇÃO (Google Maps)".PadRight(40));
+
+        while (reader1.Read())
+        {
+
+            string READ1 = reader1.GetString(0);
+            string READ2 = reader1.GetString(1);
+            string READ3 = reader1.GetString(2);
+
+
+            Console.WriteLine("");
+
+
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            Console.Write(">    ");
+            Console.Write(READ1.PadRight(40, '.'));
+            Console.Write(READ2.PadRight(40, '.'));
+            Console.Write(READ3.PadRight(40));
+            Console.Write(Environment.NewLine);
+        }
+        //Console.Write("Nome".PadRight(30, '.'));
+
+        oficinaList.Close();
+
+        Console.ResetColor();
+        Console.Write(Environment.NewLine);
+        Console.Write(Environment.NewLine);
+        Console.WriteLine("Pressione qualquer tecla para continuar");
+        Console.ReadLine();
+
+
+    }
+
+    //falta acabar a parte de seleção de oficina > no caso colocar uma opção de selecionar oficina e
+    // dai redirecionar para os agendamentos atraves de um goto;
+
+
+    if (opcaoSelecionada == "2")
+    {
+        //fazer
+    }
+
+    if (opcaoSelecionada == "3") 
+    { 
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Precisa de uma ajuda com o veículo?  Conte-nos o que aconteceu:");
+        
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+        Console.WriteLine("1 > O carro não dá partida");
+    }
+
+    if (opcaoSelecionada == "4") { }
+
+    if (opcaoSelecionada == "5") { }
+
+    if (opcaoSelecionada == "6") { }
+
+    if (opcaoSelecionada == "0")
+    {
+        break;
+    }
+
 }
